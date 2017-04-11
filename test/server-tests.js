f387465727e0b65e54d57ec4e63aac03f93026ce
TheSaintMichael <MichaelQuiban@outlook.com>
2017-04-10 23:34:31 -0700

    Scaffolding for server, and environment

---
+++ test/server-tests.js
@@ -0,0 +1,25 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+const {app, runServer, closeServer} = require('../server');
+
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Server', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should return 200', function() {
+    return chai.request(app)
+      .get('/server')
+      .then(function(res) {
+        res.should.have.status(200);
+      });
+  });
+});
