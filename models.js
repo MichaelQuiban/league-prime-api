2a9d76e6b7e069e2f685f7958f9f87e7f58db79c
TheSaintMichael <MichaelQuiban@outlook.com>
2017-04-24 23:15:42 -0700

    Defined schema for champion data tracker

--- models.js
+++ models.js
@@ -4,15 +4,25 @@ const Schema = mongoose.Schema;
 //User information
 const loginSchema = new Schema({
 	username: { type: String, required: true, unique: true},
-	password: { type: String, required: true},
+	password: { type: String, required: true}
 })
 
-const playerSchema = new Schema({
-	summoner: { type: String, required: true,}
+//Champion Tracking Data
+const championSchema = new Schema({
+	name: {type: String, required: true, unique: true},
+	build: {type: String},
+	lane: {type: String, required: true},
+	wins: {type: String, required: true},
+	loss: {type: String, required: true}
+	ranked: {type: Boolean},
+	damage: {type: String},
+	kills: {type: String},
+	deaths: {type: String}
 })
 
+//Create a model using the schema data.
 const Login = mongoose.model('Login', loginSchema);
-const Player = mongoose.model('Player', playerSchema);
+const Champion = mongoose.model('Champion', championSchema;
 
 //Make this available to our users in the League-Prime-Api
 module.exports = Login;
